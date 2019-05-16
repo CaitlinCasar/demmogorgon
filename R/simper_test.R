e6e9a912c8a79696b57144335b25d0426fc8d6a9
Caitlin <casar@u.northwestern.edu>
2019-05-16 18:37:55 -0500

    adding simper function

--- R/simper_test.R
+++ R/simper_test.R
@@ -1,12 +1,7 @@
-simper_test <- function(data, metadata){
-  simper_test_data <- vegan::simper(data, metadata$site)
-  simper_test_data <-summary(simper_test_data)
-  comparisons <- names(simper_test_data)
+simper_test <- function(data, metadata, comparison_index=2, num_perm=1, p_threshold=1){
+  simper_test_data <- vegan::simper(data, metadata[,comparison_index], permutations = num_perm)
+  test <- as.data.frame(do.call(cbind, purrr::flatten(simper_test_data)))
+  test <- sapply(unique(names(test)), function(x) unname(unlist(test[,names(test)==x])))
+  test <- cbind(comparisons=names(rep(simper_test_data, each=ncol(data))), test)
+  as.data.frame(test, stringsAsFactors = FALSE) %>% dplyr::filter(p<=p_threshold)
 }
-
-
-# editing area ------------------------------------------------------------
-
-#return average between-group dissimilarity. This is the sum of the item average
-#sim_test$D1_D2$overall
-#need to figure out how to iterate over comparisons and return overall
